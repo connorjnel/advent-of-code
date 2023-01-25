6cf8f460eedac6771c852a7e7923428dd0bc9262
connorjnel <connor.j.nel00@gmail.com>
2023-01-25 12:45:57 +0000

    day 2 completed

--- 2021/day-2.js
+++ 2021/day-2.js
@@ -1045,10 +1045,87 @@ let depth_position = 0;
 function navigate() {
 	input.forEach((direction) => {
 		if (direction.includes('forward')) {
-			let num = Number(direction.slice(8));
-			console.log(num);
+			let horizontal = Number(direction.slice(8));
+			horizontal_position += horizontal;
+			return horizontal_position;
+		} else if (direction.includes('up')) {
+			let depthUp = Number(direction.slice(3));
+			depth_position -= depthUp;
+			return depth_position;
+		} else if (direction.includes('down')) {
+			let depthDown = Number(direction.slice(5));
+			depth_position += depthDown;
+			return depth_position;
 		}
 	});
+	console.log(`Position = ${horizontal_position * depth_position}`);
 }
 
 navigate();
+
+// Answer 1714950.  Bit annoying how input data is presented but can edit using vscode, although using pure js method to parse data may be better practice
+
+// Part 2
+
+/*
+
+--- Part Two ---
+
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+    forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    down 5 adds 5 to your aim, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    up 3 decreases your aim by 3, resulting in a value of 2.
+    down 8 adds 8 to your aim, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+*/
+
+let horizon = 0;
+let depth = 0;
+let aim = 0;
+
+function navigateSmart() {
+	input.forEach((direction) => {
+		if (direction.includes('down')) {
+			let down = Number(direction.slice(5));
+			aim += down;
+			return aim;
+		}
+
+		if (direction.includes('up')) {
+			let up = Number(direction.slice(3));
+			aim -= up;
+			return aim;
+		}
+
+		if (direction.includes('forward')) {
+			let x = Number(direction.slice(8));
+			horizon += x;
+			depth += aim * x;
+			return horizon, depth, aim;
+		}
+	});
+	console.log(`Final Position = ${horizon * depth}`);
+}
+
+navigateSmart();
+
+// Answer is 1281977850, had a bit of trouble with final forward calculation, needed to add aim * x to depth, not just declare it as depth value
